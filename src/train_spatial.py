133401648c07aa22a9a3ffa575dd862de9ff5a08
tcl326 <ouyangjiahong13@gmail.com>
2018-04-08 21:20:37 -0400

    start training version for spatial attention model

--- src/train_spatial.py
+++ src/train_spatial.py
@@ -3,6 +3,7 @@ import os
 import time
 import torch
 import pdb
+import shutil
 import torch.utils.model_zoo as model_zoo
 from torch.nn.parameter import Parameter
 import torchvision.models as models
@@ -26,22 +27,22 @@ def train(train_data, extractor_model, model, criterion, optimizer, epoch, logge
 
     print("training interaction number")
     train_num = len(train_data)
-    train_num = 2
+    # train_num = 2
     # print(train_num)
     end = time.time()
     for i in range(train_num):
         # get data, img_seq: (ts,224,224,3), gaze_seq: (ts, 3), ouput: (ts, 6)
         [img_seq, gaze_seq], target_seq = next(train_data)
-        img_seq = img_seq[:30]          # just for speed up
-        gaze_seq = gaze_seq[:30]
-        target_seq = target_seq[:30]
+        # img_seq = img_seq[:30]          # just for speed up
+        # gaze_seq = gaze_seq[:30]
+        # target_seq = target_seq[:30]
         ts = img_seq.shape[0]
 
         img_seq = normalize(img_seq)
         img_seq = np.reshape(img_seq, (bs*ts,3,) + img_size)
-        img_seq_var = torch.autograd.Variable(torch.Tensor(img_seq), requires_grad=True)
-        gaze_seq_var = torch.autograd.Variable(torch.Tensor(gaze_seq), requires_grad=True)
-        target_seq_var = torch.autograd.Variable(torch.Tensor(target_seq)).long()
+        img_seq_var = torch.autograd.Variable(torch.Tensor(img_seq).cuda(), requires_grad=True)
+        gaze_seq_var = torch.autograd.Variable(torch.Tensor(gaze_seq).cuda(), requires_grad=True)
+        target_seq_var = torch.autograd.Variable(torch.Tensor(target_seq).cuda()).long()
         gaze_seq_var = gaze_seq_var.unsqueeze(0)        # no bs dim
 
         # extract cnn feature
@@ -61,7 +62,7 @@ def train(train_data, extractor_model, model, criterion, optimizer, epoch, logge
         target_seq_var = target_seq_var.view(bs*ts)
 
         loss = criterion(prediction, target_seq_var)
-        print(loss.data)
+        # print(loss.data)
         loss.backward()
         optimizer.step()
         time_cnt = time.time() - end
@@ -91,7 +92,7 @@ def validate(val_data, extractor_model, model, criterion, epoch, logger, para):
 
     print("validation interaction number")
     val_num = len(val_data)
-    val_num = 2
+    # val_num = 2
     # print(val_num)
     end = time.time()
     loss_sum = 0
@@ -108,9 +109,9 @@ def validate(val_data, extractor_model, model, criterion, epoch, logger, para):
 
         img_seq = normalize(img_seq)
         img_seq = np.reshape(img_seq, (bs*ts,3,) + img_size)
-        img_seq_var = torch.autograd.Variable(torch.Tensor(img_seq))
-        gaze_seq_var = torch.autograd.Variable(torch.Tensor(gaze_seq))
-        target_seq_var = torch.autograd.Variable(torch.Tensor(target_seq)).long()
+        img_seq_var = torch.autograd.Variable(torch.Tensor(img_seq).cuda())
+        gaze_seq_var = torch.autograd.Variable(torch.Tensor(gaze_seq).cuda())
+        target_seq_var = torch.autograd.Variable(torch.Tensor(target_seq).cuda()).long()
         gaze_seq_var = gaze_seq_var.unsqueeze(0)        # no bs dim
 
         # extract cnn feature
@@ -145,6 +146,7 @@ def validate(val_data, extractor_model, model, criterion, epoch, logger, para):
            epoch, i, val_num, time_cnt=time_cnt, loss=loss_avg, acc=acc_avg))
     logger.scalar_summary('val/loss', loss_avg, epoch)
     logger.scalar_summary('val/acc', acc_avg, epoch)
+    return acc_avg
 
 def metric_frame(output, target):
     correct = 0
@@ -161,7 +163,7 @@ def main():
     num_class = 6
     batch_size = 1
     time_step = 32
-    epochs = 20
+    epochs = 50
     cnn_feat_size = 256     # AlexNet
     gaze_size = 3
     gaze_lstm_hidden_size = 64
@@ -169,9 +171,10 @@ def main():
     learning_rate = 0.0001
     momentum = 0.9
     weight_decay = 1e-4
-    eval_freq = 2
-    print_freq = 1
-    dataset_path = '../data/gaze_dataset'
+    eval_freq = 1       # epoch
+    print_freq = 1      # iteration
+    # dataset_path = '../data/gaze_dataset'
+    dataset_path = '../../gaze-net/gaze_dataset'
     img_size = (224, 224)
     log_path = '../log'
     logger = Logger(log_path, 'spatial')
@@ -180,27 +183,27 @@ def main():
     arch = 'alexnet'
     extractor_model = FeatureExtractor(arch=arch)
     extractor_model.features = torch.nn.DataParallel(extractor_model.features)
-    # extractor_model.cuda()      # uncomment this line if using cpu
+    extractor_model.cuda()      # uncomment this line if using cpu
     extractor_model.eval()
 
     model = SpatialAttentionModel(num_class, cnn_feat_size,
                         gaze_size, gaze_lstm_hidden_size, gaze_lstm_projected_size)
-    # model.cuda()
+    model.cuda()
 
     # define loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    # criterion = nn.CrossEntropyLoss().cuda()
+    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss().cuda()
     optimizer = torch.optim.SGD(model.parameters(), learning_rate,
                                 momentum = momentum, weight_decay=weight_decay)
 
     # define generator
-    trainGenerator = gaze_gen.GazeDataGenerator(validation_split=0)
+    trainGenerator = gaze_gen.GazeDataGenerator(validation_split=0.2)
     train_data = trainGenerator.flow_from_directory(dataset_path, subset='training',
                     batch_size=batch_size, target_size= img_size, class_mode='sequence_pytorch')
     # small dataset, error using validation split
-    # val_data = trainGenerator.flow_from_directory(dataset_path, subset='validation',
-    #             batch_size=batch_size, target_size= img_size, class_mode='sequence')
-    val_data = train_data
+    val_data = trainGenerator.flow_from_directory(dataset_path, subset='validation',
+                batch_size=batch_size, target_size= img_size, class_mode='sequence_pytorch')
+    # val_data = train_data
 
     # img_seq: (ts,224,224,3), gaze_seq: (ts, 3), ouput: (ts, 6)
     # [img_seq, gaze_seq], output = next(train_data)
@@ -212,7 +215,9 @@ def main():
     # start Training
     para = {'bs': batch_size, 'img_size': img_size, 'num_class': num_class,
             'print_freq': print_freq}
-    validate(val_data, extractor_model, model, criterion, 0, logger, para)
+    best_acc = 0
+    # validate(val_data, extractor_model, model, criterion, 0, logger, para)
+
     for epoch in range(epochs):
         adjust_learning_rate(optimizer, epoch, learning_rate)
         print 'Epoch: {}'.format(epoch)
@@ -221,7 +226,16 @@ def main():
 
         # evaluate on validation set
         if epoch % eval_freq == 0 or epoch == epochs - 1:
-            validate(val_data, extractor_model, model, criterion, epoch, logger, para)
+            acc = validate(val_data, extractor_model, model, criterion, epoch, logger, para)
+            is_best = acc > best_acc
+            best_acc = max(acc, best_acc)
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': arch,
+                'state_dict': model.state_dict(),
+                'best_acc': best_acc,
+                'optimizer': optimizer.state_dict(),
+            }, is_best)
 
 
 def adjust_learning_rate(optimizer, epoch, learning_rate):
@@ -229,6 +243,12 @@ def adjust_learning_rate(optimizer, epoch, learning_rate):
     lr = learning_rate * (0.1 ** (epoch // 30))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+
+def save_checkpoint(state, is_best, filename='../model/spatial/checkpoint.pth.tar'):
+    print("save checkpoint")
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, '../model/spatial/model_best.pth.tar')
 
 
 if __name__ == '__main__':
